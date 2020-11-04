1efd638d06457916979b662b85addf6ab4452d1f
Huxiaotong8659 <1711037470@qq.com>
2020-11-04 16:18:42 +0800

    第九周课程

---
+++ web课程7/apple.js
@@ -0,0 +1,11 @@
+function apple(){
+    //var let
+    var array=[28,65,89,100,34,56,78,54];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+    }
+}
+
+
+apple();
