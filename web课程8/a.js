e113de40ff64333870a7d60bbf5d76def0cd9349
Huxiaotong8659 <1711037470@qq.com>
2020-11-18 23:29:59 +0800

    第11周第二次课

---
+++ web课程8/a.js
@@ -0,0 +1,13 @@
+var apple = function(){
+    console.log('error');
+}
+
+apple()
+
+function orange(){
+    console.log('error orange');
+    function redOrange(){
+        console.log('')
+    }
+}
+orange()
